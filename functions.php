1af43b178cc20908f055c60d864f782fdc5dfa81
Bilel <b.oueslati@ewm.swiss>
2022-09-10 15:53:01 +0200

    Update

--- functions.php
+++ functions.php
@@ -1,8 +1,15 @@
 <?php
-add_action('rest_api_init', function() {
+
+add_theme_support('post-thumbnails');
+
+add_action('rest_api_init', function () {
     remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
 }, 15);
 
-add_filter('rest_url', function($url) {
-        return str_replace(home_url(), site_url(), $url);
+add_filter('rest_url', function ($url) {
+    return str_replace(home_url(), site_url(), $url);
 });
+
+header("Access-Control-Allow-Origin: *");
+
+
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $api = get_rest_url();
 
 wp_send_json_success(["api_url" => $api]);
