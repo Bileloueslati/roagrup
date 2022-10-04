148db762a5c641e43dbd2d586a00c772b81394ac
Bilel <b.oueslati@ewm.swiss>
2022-10-04 21:16:53 +0200

    Remove Acf action : show in api

--- src/Acf/Acf.php
+++ src/Acf/Acf.php
@@ -31,7 +31,7 @@ class Acf
 
     public static function handleAcfApi()
     {
-        add_filter('acf/rest_api/field_settings/show_in_rest', '__return_true');
+        // add_filter('acf/rest_api/field_settings/show_in_rest', '__return_true');
 
         // Enable the option show in rest
         // add_filter('acf/rest_api/field_settings/show_in_rest', '__return_true');
